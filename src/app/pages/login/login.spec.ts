abe0eb0165b9ce1eac6abef0a275c45df4b4cd5b
Jose Elias <josejosephelias@gmail.com>
2025-07-26 10:28:22 +0300

    signup done but need to work on the interceptor and guard

--- src/app/pages/login/login.spec.ts
+++ src/app/pages/login/login.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Login } from './login';
+import { AuthService } from '../../core/authentication/auth';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { throwError } from 'rxjs';
 
 describe('Login', () => {
   let component: Login;
   let fixture: ComponentFixture<Login>;
+  let mockAuthService: any;
+  let mockRouter: any;
 
   beforeEach(async () => {
+    mockAuthService = {
+      login: jasmine
+        .createSpy('login')
+        .and.returnValue(throwError(() => new Error('Unauthorized'))),
+    };
+
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate'),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [Login]
-    })
-    .compileComponents();
+      declarations: [Login],
+      imports: [ReactiveFormsModule],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: Router, useValue: mockRouter },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
@@ -19,5 +38,17 @@ describe('Login', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should NOT take you to homepage if not logged in', () => {
+    component.loginForm.setValue({
+      email: 'wrong@example.com',
+      password: 'wrongpass',
+    });
+
+    component.loginAcc();
+
+    expect(mockAuthService.login).toHaveBeenCalled();
+    expect(mockRouter.navigate).not.toHaveBeenCalled();
   });
 });
